439fc0da13b15b92badccceeda7d5d93e1dd97fe
小丶浣 <78002376+xiaiohuan@users.noreply.github.com>
2022-02-28 21:54:37 +0800

    Update board.h

--- trunk/configs/boards/HC5962/board.h
+++ trunk/configs/boards/HC5962/board.h
@@ -1,6 +1,6 @@
 /* B70 */
 
-#define BOARD_PID		"B70"
+#define BOARD_PID		"HIWIFI"
 #define BOARD_NAME		"HIWIFI"
 #define BOARD_DESC		"HIWIFI Wireless Router"
 #define BOARD_VENDOR_NAME	"HiWiFi/Gee Technology"
